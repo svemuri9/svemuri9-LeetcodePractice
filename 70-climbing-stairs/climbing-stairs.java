8e3a0cafa2547900746fbf394a0729f736cdc1f9
Srinivasa Vamsi Bhargav Vemuri <141910546+svemuri9@users.noreply.github.com>
2024-06-16 05:36:19 -0700

    Time: 0 ms (100.00%) | Memory: 40.2 MB (38.96%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n==1) return 1;
+        if (n==2) return 2;
+       int []memo = new int[n];
+       memo [0]=1;
+       memo [1]=2;
+
+       for (int i =2; i< n;i++)
+       {
+            memo[i]= memo[i-1]+memo[i-2];
+       }
+       return memo[n-1];        
+    }
+}
