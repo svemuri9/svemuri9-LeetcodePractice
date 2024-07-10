feb93d7a02ec570825f1cf692446dc9531771685
Srinivasa Vamsi Bhargav Vemuri <141910546+svemuri9@users.noreply.github.com>
2024-07-09 17:31:00 -0700

    Time: 1 ms (99.42%) | Memory: 41.8 MB (81.60%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minOperations(String[] logs) {
         int count =0;
-        for (var log: logs) {
+        for (String log: logs) {
       switch (log) {
         case "../":
           count = Math.max(0, count - 1);
