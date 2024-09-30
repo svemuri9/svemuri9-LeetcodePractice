08f123da8417c9e8f95498f5bff02cf39e1ef45c
Srinivasa Vamsi Bhargav Vemuri <141910546+svemuri9@users.noreply.github.com>
2024-09-30 04:42:58 -0700

    Time: 5 ms (93.47%) | Memory: 45.7 MB (42.51%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
@@ -1,28 +1,21 @@
 class CustomStack {
-
-    // Array to store stack elements
     private int[] stackArray;
-    // Index of the top element in the stack
     private int topIndex;
-
     public CustomStack(int maxSize) {
         stackArray = new int[maxSize];
         topIndex = -1;
     }
-
     public void push(int x) {
         if (topIndex < stackArray.length - 1) {
             stackArray[++topIndex] = x;
         }
     }
-
     public int pop() {
         if (topIndex >= 0) {
             return stackArray[topIndex--];
         }
-        return -1; // Return -1 if the stack is empty
+        return -1; 
     }
-
     public void increment(int k, int val) {
         int limit = Math.min(k, topIndex + 1);
         for (int i = 0; i < limit; i++) {
